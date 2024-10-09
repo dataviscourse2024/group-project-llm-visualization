1ecdf13cec253cde6518fef2224e345533c97a36
Jake Wagoner <jakewagoneredu@gmail.com>
2024-10-09 14:00:11 -0600

    Frontend infrastructure initial setup

---
+++ web/src/components/VisContainer.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import LineChart from './visualizations/LineChart.vue';
+</script>
+
+<template>
+  <div class="vis-container">
+    <LineChart />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
