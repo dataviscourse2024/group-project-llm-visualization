3bf5982c3a54029c3445400d7e2fa56fb842f502
Jake Wagoner <jakewagoneredu@gmail.com>
2024-11-14 15:42:22 -0700

    Some frontend style changes

--- web/src/components/VisContainer.vue
+++ web/src/components/VisContainer.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="vis-container">
-    <v-container fluid class="fill-height">
-      <v-row class="fill-height">
-        <v-col class="chart-container">
+    <v-container class="fill-height">
+      <v-col class="chart-container">
+        <v-row class="fill-height">
           <LineChart :data="chartData" />
-        </v-col>
-      </v-row>
-      <FetchButton @data-fetched="updateChartData" /> <!-- Listen for data-fetched -->
+        </v-row>
+        <v-row>
+          <FetchButton @data-fetched="updateChartData" /> <!-- Listen for data-fetched -->
+        </v-row>
+      </v-col>
     </v-container>
   </div>
 </template>
+
+<style scoped>
+  .vis-container {
+    height: 100%;
+  }
+</style>
 
 <script setup lang="ts">
 import LineChart from './visualizations/LineChart.vue';
