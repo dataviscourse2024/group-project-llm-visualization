3fd1f3acca4d99dc556d7e5efd193054c0871420
Jake Wagoner <jakewagoneredu@gmail.com>
2024-11-14 12:05:22 -0700

    Update LLM sidebar to include mock-api/bot response and framework for api calls

--- web/src/components/LLMSidebar.vue
+++ web/src/components/LLMSidebar.vue
@@ -3,25 +3,72 @@ import { computed, ref } from 'vue';
 
 const open = ref(false);
 const userMessages = ref<{[date: number]: string}>({});
-const llmMessages = ref<{[date: number]: string}>({12: "# Lorem Ipsum is \n\n\nsimply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-  13: "*test*"
+const llmMessages = ref<{[date: number]: string}>({0: "# Lorem Ipsum is \n\n\nsimply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+  1: "*test*"
 });
-const messages = computed(() => {return {...userMessages.value, ...llmMessages.value}});
+const messages = computed(() => {
+  const combinedMessages = {...userMessages.value, ...llmMessages.value};
+  return Object.keys(combinedMessages)
+    .sort((a, b) => Number(a) - Number(b))
+    .reduce((acc: any, key: any) => {
+      acc[key] = combinedMessages[key];
+      return acc;
+    }, {});
+});
 const newMessage = ref('');
 
-// TODO: implement API call to LLM
-const sendMessage = () => {
-  if (newMessage.value.trim() !== '') {
-    const date = new Date();
-    userMessages.value[date.getTime()] = (newMessage.value);
-    newMessage.value = '';
-    // Scroll to the bottom of the messages
-    setTimeout(() => {
+const responseLoading = ref(false);
+
+const currentTime = computed(() => Date.now());
+
+const scrollChatBox = (timeout: number = 100) => {
+  setTimeout(() => {
       const messageContainer = document.querySelector('.messages');
       if (messageContainer) {
         messageContainer.scrollTop = messageContainer.scrollHeight;
       }
-    }, 100);
+    }, timeout);
+}
+
+async function sendMessageToLLM(message: string) {
+  // send message to LLM
+  // api call here (should be async)
+  // simulate response (TODO: actual response)
+  await new Promise((resolve, reject) => {
+    // TODO: simulated error
+    if (message === 'error') {
+      reject('Error message');
+    }
+    setTimeout(() => {
+      llmMessages.value[currentTime.value] = "I'm a bot, you said: " + message;
+      resolve(true);
+    }, 2000);
+  });
+}
+
+// TODO: implement API call to LLM
+async function sendMessage() {
+  if (newMessage.value.trim() !== '') {
+    // Create a new message object in order to clear the chatbox (looks better when sending)
+    const m = newMessage.value;
+    newMessage.value = '';
+    userMessages.value[currentTime.value] = (m);
+    // Scroll to the bottom of the messages
+    scrollChatBox();
+
+    // send message to LLM
+    // api call here (should be async)
+    responseLoading.value = true;
+
+    sendMessageToLLM(m).then(() => {
+      console.log('Message sent to LLM');
+      responseLoading.value = false;
+      scrollChatBox();
+    }).catch((error) => {
+      console.error('Failed to send message to LLM:', error);
+      llmMessages.value[currentTime.value] = "There was an error loading a response...";
+      responseLoading.value = false;
+    });
   }
 };
 </script>
@@ -36,6 +83,9 @@ const sendMessage = () => {
     <div class="chat-container">
       <h2 class="sidebar-title">LLM Chat</h2>
       <div class="messages">
+        <div v-if="responseLoading" class="overlay">
+          <v-progress-circular indeterminate color="primary"></v-progress-circular>
+        </div>
         <div 
           v-for="([time, message], index) in Object.entries(messages)" 
           :key="index" 
@@ -83,6 +133,18 @@ const sendMessage = () => {
 }
 .toggle-btn.open {
     right: 395px; 
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .chat-container {
