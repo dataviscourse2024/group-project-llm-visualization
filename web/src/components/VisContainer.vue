583499a5a7a3b963dd66b232a12b8782331c1381
Tin Vo <tinvout@outlook.com>
2024-11-20 20:19:00 -0700

    Added setup for map vis

--- web/src/components/VisContainer.vue
+++ web/src/components/VisContainer.vue
@@ -3,9 +3,9 @@
     <v-container class="fill-height">
       <v-col class="chart-container">
         <v-row class="fill-height">
-          <LineChart :data="filteredChartData" />
+          <component :is="currentViewComponent" :data="filteredChartData" />
         </v-row>
-        <v-row>
+        <v-row v-if="currentView.value === 'lineChart'">
           <TimeSlider :data="chartData" @updateWindow="updateWindow" />
         </v-row>
         <v-row>
@@ -24,12 +24,18 @@
 
 <script setup lang="ts">
 import LineChart from './visualizations/LineChart.vue';
+import Map from './visualizations/Map.vue';
 import FetchButton from './FetchButton.vue';
 import TimeSlider from './TimeSlider.vue';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+import { currentView } from './state'; // Import the global ref
 
 const chartData = ref([]);
 const filteredChartData = ref([]);
+
+const currentViewComponent = computed(() => {
+  return currentView.value === 'lineChart' ? LineChart : Map;
+});
 
 const updateChartData = (data) => {
   chartData.value = data;
